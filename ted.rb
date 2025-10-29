9913da01dc86f0cac69e287ac5fe7b90b6fd128a
Eric Feng <erichfeng@gmail.com>
2025-10-29 15:17:14 -0700

    v0.1.9

--- ted.rb
+++ ted.rb
@@ -5,16 +5,16 @@
 class Ted < Formula
   desc "A Go binary utility"
   homepage "https://github.com/ehfeng/ted"
-  version "0.1.8"
+  version "0.1.9"
 
   on_macos do
     on_arm do
       url "https://github.com/ehfeng/ted/releases/download/v#{version}/ted_#{version}_Darwin_arm64.tar.gz"
-      sha256 "fe41e61eefb3e50d89670efa7c579aec2c6733fa74f0f2c10d9d94f4b76cb571"
+      sha256 "9a694d0f81fca82f36ec0b32ab7a3e3551df30e4013727a7bed097629593be43"
     end
     on_intel do
       url "https://github.com/ehfeng/ted/releases/download/v#{version}/ted_#{version}_Darwin_x86_64.tar.gz"
-      sha256 "73d3386ed64a39f53f6ac1fefcc80d2a8b89b80c8a7875ac4e64ad3ccf4f6306"
+      sha256 "d940021670107e4cb21cd8dc98641d274ad3949f924ab8d32fe378db613f957a"
     end
   end
 
@@ -25,7 +25,7 @@ class Ted < Formula
     end
     on_intel do
       url "https://github.com/ehfeng/ted/releases/download/v#{version}/ted_#{version}_Linux_x86_64.tar.gz"
-      sha256 "c0804276752b70cc8440852a26216fd8127fde027f1b7fa1ae09722ba4764f17"
+      sha256 "b4718214929184feffdb6f42c59f8d46ea8832ef564c720cedd9f9c0fd533a79"
     end
   end
 
